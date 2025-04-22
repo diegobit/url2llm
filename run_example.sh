b57baa43f0a044d2ba22b9992f10a67f1c4e6d08
Diego Giorgini <diego.giorgini@icloud.com>
2025-04-22 18:49:19 +0200

    change defaults

--- run_example.sh
+++ run_example.sh
@@ -1,8 +1,8 @@
 uv run main.py \
-   --url "https://modelcontextprotocol.io/llms.txt" \
-   --depth 1 \
+   --url "https://modelcontextprotocol.io/" \
+   --depth 2 \
    --instruction "I need documents related to developing MCP (model context protocol) servers" \
    --provider "gemini/gemini-2.5-flash-preview-04-17" \
    --api_key ${GEMINI_API_KEY} \
    --concurrency 32 \
-   --output-dir ./md_out
+   --output-dir ~/Desktop/crawl_out/
